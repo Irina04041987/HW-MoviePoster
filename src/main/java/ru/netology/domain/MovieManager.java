d41e13b6f11fdb47428e567dc9c4c8aa12f09194
Irina Platonova <crazyangeli@yandex.ru>
2021-02-17 15:15:01 +0300

    add MovieManager.java

---
+++ src/main/java/ru/netology/domain/MovieManager.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class MovieManager {
+
+    private MovieItems[] movies = new MovieItems[0];
+
+    public void add(MovieItems movie) {
+    }
+
+    public MovieItems[] getAll() {
+        MovieItems[] result = new MovieItems[0];
+        return result;
+    }
+
+}
